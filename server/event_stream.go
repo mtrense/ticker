a296b77a1b464aae18f1599235339a355d0f5bed
Max Trense <dev@trense.info>
2021-03-19 00:20:16 +0100

    Add logic for emitting Events over the RPC adapter

--- server/event_stream.go
+++ server/event_stream.go
@@ -3,6 +3,10 @@ package server
 import (
 	"context"
 
+	"github.com/golang/protobuf/ptypes"
+
+	"github.com/mtrense/ticker/eventstream/base"
+
 	"github.com/mtrense/ticker/rpc"
 )
 
@@ -11,8 +15,23 @@ type eventStreamServer struct {
 	server *Server
 }
 
-func (s *eventStreamServer) Emit(context.Context, *rpc.Event) (*rpc.Ack, error) {
-	return &rpc.Ack{}, nil
+func (s *eventStreamServer) Emit(ctx context.Context, event *rpc.Event) (*rpc.Ack, error) {
+	occurredAt, err := ptypes.Timestamp(event.OccurredAt)
+	if err != nil {
+		return nil, err
+	}
+	payload := event.Payload.AsMap()
+	ev := base.Event{
+		Sequence:   -1,
+		Aggregate:  event.Aggregate,
+		Type:       event.Type,
+		OccurredAt: occurredAt,
+		Payload:    payload,
+	}
+	seq, err := s.server.streamBackend.Store(&ev)
+	return &rpc.Ack{
+		Sequence: int64(seq),
+	}, err
 }
 
 func (s *eventStreamServer) Subscribe(subscription *rpc.Subscription, stream rpc.EventStream_SubscribeServer) error {
