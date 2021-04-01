f067e7994cd1b346ac7ef74e611303ce3b410241
Max Trense <dev@trense.info>
2021-04-01 20:43:54 +0200

    Add helpers for Selectors

--- eventstream/base/selector.go
+++ eventstream/base/selector.go
@@ -1,8 +1,44 @@
 package base
 
+import (
+	"errors"
+	"strings"
+)
+
 type Selector struct {
 	Aggregate []string
 	Type      string
+}
+
+func ParseSelector(s string) (*Selector, error) {
+	splitSelector := strings.Split(s, "/")
+	if len(splitSelector) > 2 {
+		return nil, errors.New("expected at most one slash")
+	}
+	aggregates := strings.Split(splitSelector[0], ".")
+	if len(splitSelector) == 2 {
+		return &Selector{
+			Aggregate: aggregates,
+			Type:      splitSelector[1],
+		}, nil
+	} else {
+		return &Selector{
+			Aggregate: aggregates,
+			Type:      "",
+		}, nil
+	}
+}
+
+func (s *Selector) IsComplete() bool {
+	if s.Type == "" {
+		return false
+	}
+	for _, a := range s.Aggregate {
+		if a == "" {
+			return false
+		}
+	}
+	return true
 }
 
 type SelectOption func(s *Selector)
