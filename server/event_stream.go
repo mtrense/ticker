8fca5e1b9afcfd8cdc84609ebc916ac546caae26
Max Trense <dev@trense.info>
2021-04-01 20:44:10 +0200

    Update EventStream service

--- server/event_stream.go
+++ server/event_stream.go
@@ -3,9 +3,11 @@ package server
 import (
 	"context"
 
+	"github.com/mtrense/soil/logging"
+
 	"github.com/golang/protobuf/ptypes"
 
-	"github.com/mtrense/ticker/eventstream/base"
+	es "github.com/mtrense/ticker/eventstream/base"
 
 	"github.com/mtrense/ticker/rpc"
 )
@@ -21,7 +23,7 @@ func (s *eventStreamServer) Emit(ctx context.Context, event *rpc.Event) (*rpc.Ac
 		return nil, err
 	}
 	payload := event.Payload.AsMap()
-	ev := base.Event{
+	ev := es.Event{
 		Sequence:   -1,
 		Aggregate:  event.Aggregate,
 		Type:       event.Type,
@@ -34,7 +36,34 @@ func (s *eventStreamServer) Emit(ctx context.Context, event *rpc.Event) (*rpc.Ac
 	}, err
 }
 
-func (s *eventStreamServer) Subscribe(subscription *rpc.Subscription, stream rpc.EventStream_SubscribeServer) error {
+func (s *eventStreamServer) Stream(req *rpc.StreamRequest, stream rpc.EventStream_StreamServer) error {
+	selector := es.Selector{
+		Aggregate: req.Selector.Aggregate,
+		Type:      req.Selector.Type,
+	}
+	bracket := es.Bracket{
+		NextSequence: req.Bracket.FirstSequence,
+		LastSequence: req.Bracket.LastSequence,
+	}
+	s.server.streamBackend.Stream(stream.Context(), selector, bracket, func(e *es.Event) {
+		ev := rpc.EventToProto(e)
+		err := stream.Send(ev)
+		if err != nil {
+			logging.L().Err(err).Msg("Couldn't send event")
+		}
+	})
+	return nil
+}
 
+func (s *eventStreamServer) Subscribe(req *rpc.SubscriptionRequest, stream rpc.EventStream_SubscribeServer) error {
+	persistentClientID := req.PersistentClientId
+	selector := es.Selector{
+		Aggregate: req.Selector.Aggregate,
+		Type:      req.Selector.Type,
+	}
+	s.server.streamBackend.Subscribe(stream.Context(), persistentClientID, selector, func(e *es.Event) {
+		ev := rpc.EventToProto(e)
+		stream.Send(ev)
+	})
 	return nil
 }
